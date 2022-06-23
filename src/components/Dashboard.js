6ee116ca51c6bf351d5c1c12906b2ff00d2d0d3c
mai <Mai9550>
2022-06-23 22:56:57 +0200

    fourth

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,24 +1,15 @@
 import React,{Component} from 'react' 
-import {  connect  } from 'react-redux'
-import {  handleinitialData  } from '../actions/shared'
-import Dashboard from './Dashboard'
 
 class Dashboard extends Component {
-    componentDidMount(){
-        this.props.dispatch(handleinitialData())
-    }
+
     render(){
         return (
-           <Dashboard/>
+            <div>
+                Dashboard
+            </div>
         )
     }
 
 }
 
-function mapStateToProps ({tweets}) {
-    return {
-        tweetIds:Object.keys(tweets).sort((a,b)=>tweets[b].timestamp - tweets[a].timestamp)
-    }
-}
-
-export default connect(mapStateToProps)(Dashboard);
+export default Dashboard;
