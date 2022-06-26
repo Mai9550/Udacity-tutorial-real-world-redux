7729755307ede70a336e74c92401a1545868cebc
mai95 <mai_saasaa@yahoo.com>
2022-06-26 17:57:52 +0200

    14th

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -8,10 +8,23 @@ import TiHeartFullOutline from 'react-icons/lib/ti/heart-full-outline'
 
 class Tweet extends Component{
     render(){
-        console.log(this.props)
+        const { tweet } = this.props;
+
+            if (tweet === null) {
+                return <p>This Tweet doesn't exist</p>
+            }
+
+            const {
+                name, avatar,timestamp, text, hasLiked, likes, replies, id, parent
+            } = tweet
+            
         return (
             <div className="tweet">
-                
+                <img 
+                src={avatar}
+                alt={`Avatar of ${name}`}
+                className='avatar'
+                />
             </div>
         )
 
