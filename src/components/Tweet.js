54f419d7084c34ce33baa69adb71b6613136b7bb
mai95 <mai_saasaa@yahoo.com>
2022-06-26 17:44:27 +0200

    13th

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,6 +1,10 @@
 import React,{Component} from "react";
 import { connect } from "react-redux";
-import { formatTweet } from '../utils/helpers';
+import { formatTweet , formatDate } from '../utils/helpers';
+import TiArrowBackOutline from 'react-icons/lib/ti/arrow-back-outline'
+import TiHeartOutline from 'react-icons/lib/ti/heart-outline'
+import TiHeartFullOutline from 'react-icons/lib/ti/heart-full-outline'
+
 
 class Tweet extends Component{
     render(){
@@ -17,9 +21,10 @@ class Tweet extends Component{
 
 function mapStateToProps ({authedUser,users,tweets},{id}) {
     const tweet = tweets[id]
+    const parentTweet = tweet ? tweets[tweet.replyingTo] : null
     return {
         authedUser,
-        tweet:formatTweet(tweet,users[tweet.author],authedUser)
+        tweet:formatTweet(tweet,users[tweet.author],authedUser,parentTweet)
     }
 }
 
