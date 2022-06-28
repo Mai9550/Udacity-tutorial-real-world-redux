0532ba69125edf0f5bc2ac38b50784113b53d403
mai95 <mai_saasaa@yahoo.com>
2022-06-28 20:34:16 +0200

    24th

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { handleinitialData } from '../actions/shared'
 import Dashboard  from './Dashboard'
+import { LoadingBar } from 'react-redux-loading'
 class App extends Component {
   componentDidMount(){
     this.props.dispatch(handleinitialData())
@@ -9,6 +10,7 @@ class App extends Component {
   render() {
     return (
       <div>
+        <LoadingBar/>
         {
           this.props.loading === true
           ? null
