e49d06e064ac765be3e7bd040058214431bd1ac2
mai95 <mai_saasaa@yahoo.com>
2022-07-05 11:53:53 +0200

    form submission event

--- src/components/NewTweet.js
+++ src/components/NewTweet.js
@@ -12,6 +12,19 @@ class NewTweet extends Component {
             })
         )
     }
+    handleSubmit = (e) => {
+        e.preventDefault()
+    
+        const { text } = this.state
+    
+        // todo: Add Tweet to Store
+    
+        console.log('New Tweet: ', text)
+    
+        this.setState(() => ({
+          text: ''
+        }))
+      }
     render(){
         return(
             <div>
