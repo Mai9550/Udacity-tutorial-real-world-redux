aa0f351d69ed2dc2e814189b7eb1e6c7b14cbf65
mai95 <mai_saasaa@yahoo.com>
2022-07-05 11:50:56 +0200

    add newTweet component

---
+++ src/components/NewTweet.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react'
+
+class NewTweet extends Component {
+    state={
+        text:''
+    }
+    handleChange = (e) =>{
+        const text = e.target.value
+        this.setState(
+            ()=>({
+                text
+            })
+        )
+    }
+    render(){
+        return(
+            <div>
+                New Tweet
+            </div>
+        )
+    }
+}
+
+export default NewTweet
