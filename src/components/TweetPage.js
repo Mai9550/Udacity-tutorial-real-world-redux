d441dd6fff26e6ac7ea658b586a7e4d1c65b1921
mai95 <mai_saasaa@yahoo.com>
2022-07-06 22:03:22 +0200

    add tweetPage component

---
+++ src/components/TweetPage.js
@@ -0,0 +1,13 @@
+import React,{Component} from "react";
+
+class TweetPage extends Component {
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default TweetPage;
