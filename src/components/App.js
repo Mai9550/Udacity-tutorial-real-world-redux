02b470bddd83f74a29058a291e4fd0d33f44207d
mai95 <mai_saasaa@yahoo.com>
2022-07-21 10:07:00 +0200

    show text

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,33 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { handleinitialData } from '../actions/shared'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Dashboard  from './Dashboard'
 import { LoadingBar } from 'react-redux-loading'
 import NewTweet from './NewTweet'
 import TweetPage from './TweetPage'
+import Nav from './Nav'
 class App extends Component {
   componentDidMount(){
     this.props.dispatch(handleinitialData())
   }
   render() {
     return (
+      <Router>
       <div>
         <LoadingBar/>
         {
           this.props.loading === true
           ? null
-          : <TweetPage match={{params: {id: '8xf0y6ziyjabvozdd253nd'}}}/>
+          : <div>
+            <Route path="/" exact component={Dashboard}/>
+            <Route path="/tweet/:id" component={TweetPage} />
+            <Route path="/new" component={NewTweet}/>
+
+            </div>
        }
       </div>
+      </Router>
     )
   }
 }
